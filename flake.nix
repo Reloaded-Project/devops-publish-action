abf1ca08cd26339d3b15c5174331aa818b2b0397
Sewer56 <sewer56lol@googlemail.com>
2025-12-04 11:07:42 +0000

    Added: Nix flake with direnv for development shell (zip, 7zz)

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Development shell with zip and 7-zip";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+  in {
+    devShells = forAllSystems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.zip
+            pkgs.unzip
+            pkgs._7zz
+          ];
+        };
+      }
+    );
+  };
+}
